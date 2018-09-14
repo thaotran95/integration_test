03f8417bcfb23d72f150f1dc9513af9fc4ce8103
thaotran95 <Your Email>
2018-09-14 15:52:55 +0300

    add source

--- clock_test/build.sh
+++ clock_test/build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 export TOOLCHAIN_aarch64=/opt/sdk/imx8dvsabreauto_wayland/x86_64-linux/usr/bin/aarch64-poky-linux
 export TOOLCHAIN_armhf=/opt/sdk/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/bin
 export TOOLCHAIN_qnx660=/opt/sdk/qnx660/host/linux/x86/usr/bin/
@@ -7,9 +9,11 @@ ${TOOLCHAIN_aarch64}/aarch64-poky-linux-g++ -o linux_imx8_aarch64 --sysroot=/opt
 cp linux_imx8_aarch64 /srv/nfsroot/integration_test/clock_test
 ${TOOLCHAIN_armhf}/arm-linux-gnueabihf-g++ -o linux_imx6_armhf -I /opt/sdk/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/arm-linux-gnueabihf/include/ -L /opt/sdk/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/arm-linux-gnueabihf/lib clock.cpp -lrt -Wall
 cp linux_imx6_armhf /srv/nfsroot/integration_test/clock_test 
+source /opt/sdk/qnx660/qnx660-env.sh
 ${TOOLCHAIN_qnx660}/QCC -o qnx660_screen_arm -I /opt/sdk/qnx660/target/qnx6/usr/include -L /opt/sdk/qnx660/target/qnx6/usr/lib clock.cpp -Wall
 cp qnx660_screen_arm /srv/nfsroot/integration_test/clock_test
 ${TOOLCHAIN_wayland}/aarch64-poky-linux-g++ -o linux_wayland_aarch64 --sysroot=/opt/sdk/poky/2.1.2/sysroots/aarch64-poky-linux -L /opt/sdk/poky/2.1.2/sysroots/x86_64-pokysdk-linux/usr/lib clock.cpp -lrt -Wall
 cp linux_wayland_aarch64 /srv/nfsroot/integration_test/clock_testx
+source /opt/sdk/qnx700/qnx700-env.sh
 ${TOOLCHAIN_qnx700}/qcc -o qnx700_screen_aarch64 -I /opt/sdk/qnx700/target/qnx7/aarch64le/usr/include -L /opt/sdk/qnx700/target/qnx7/aarch64le/usr/lib clock.cpp -Wall
 cp qnx700_screen_aarch64 /srv/nfsroot/integration_test/clock_test
