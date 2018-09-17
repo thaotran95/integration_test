2571c9310f05e63c1d323cc7c548c872faf63257
thaotran95 <Your Email>
2018-09-17 10:45:19 +0300

    change name

---
+++ egl_test/build_linux.sh
@@ -0,0 +1,6 @@
+export TOOLCHAIN_aarch64=/opt/sdk/imx8dvsabreauto_wayland/x86_64-linux/usr/bin/aarch64-poky-linux
+export TOOLCHAIN_armhf=/opt/sdk/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/bin
+${TOOLCHAIN_aarch64}/aarch64-poky-linux-g++ -o linux_imx8_aarch64 --sysroot=/opt/sdk/imx8dvsabreauto_wayland/imx8dvsabreauto/ -DLINUX -DEGL_API_FB -I /opt/sdk/imx8dvsabreauto_wayland/imx8dvsabreauto/usr/include/ -L/opt/sdk/imx8dvsabreauto_wayland/imx8dvsabreauto/usr/lib test.cpp -lGLESv2 -lEGL -lGAL -lm -ldl -Wall
+cp linux_imx8_aarch64 /srv/nfsroot/integration_test/egl_test
+${TOOLCHAIN_armhf}/arm-linux-gnueabihf-g++ -o linux_imx6_armhf --sysroot=/opt/sdk/renesas-targetfs -DLINUX -DEGL_API_FB -I /opt/sdk/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/arm-linux-gnueabihf/include/ -L /opt/sdk/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/arm-linux-gnueabihf/lib -L /opt/sdk/renesas-targetfs/lib test.cpp -lGLESv2 -lEGL -lGAL -lm -ldl -Wall
+cp linux_imx6_armhf /srv/nfsroot/integration_test/egl_test
